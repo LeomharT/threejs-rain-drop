87f5a21c3f273a19ee5f10ae46a2191f07676093
LeomharT <leo.liao.lzy@gmail.com>
2025-06-11 14:52:02 +0800

    feat: add textures resource

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,107 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {
+	Color,
+	IcosahedronGeometry,
+	Mesh,
+	MeshBasicMaterial,
+	PerspectiveCamera,
+	Scene,
+	ShaderMaterial,
+	SphereGeometry,
+	Uniform,
+	Vector3,
+	WebGLRenderer,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import earthFragmentShader from './shader/earth/fragment.glsl?raw';
+import earthVertexShader from './shader/earth/vertex.glsl?raw';
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const main = document.querySelector('#app') as HTMLDivElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const size = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+	pixelRatio: Math.min(2, window.devicePixelRatio),
+};
+
+/**
+ * Basic
+ */
+
+const renderer = new WebGLRenderer({
+	alpha: true,
+	antialias: true,
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(size.pixelRatio);
+main.append(renderer.domElement);
+
+const scene = new Scene();
+scene.background = new Color('#1e1e1e');
+
+const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
+camera.position.set(3, 3, 3);
+camera.lookAt(scene.position);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const stats = new Stats();
+main.append(stats.dom);
+
+/**
+ * Scene
+ */
+
+// Uniforms
+const uniforms = {
+	uSunDirection: new Uniform(new Vector3()),
+};
+
+// Sun
+const sunGeometrt = new IcosahedronGeometry(0.1, 5);
+const sunMaterial = new MeshBasicMaterial({ color: 'yellow' });
+const sun = new Mesh(sunGeometrt, sunMaterial);
+
+scene.add(sun);
+
+function updateSun() {}
+
+// Earth
+const earthGeometry = new SphereGeometry(2, 32, 32);
+const earthMaterial = new ShaderMaterial({
+	uniforms,
+	vertexShader: earthVertexShader,
+	fragmentShader: earthFragmentShader,
+	transparent: true,
+});
+const earth = new Mesh(earthGeometry, earthMaterial);
+
+scene.add(earth);
+
+/**
+ * Events
+ */
+
+function render(time: number = 0) {
+	requestAnimationFrame(render);
+
+	stats.update();
+	controls.update(time);
+
+	renderer.render(scene, camera);
+}
+render();
+
+function resize() {
+	size.width = window.innerWidth;
+	size.height = window.innerHeight;
+	size.pixelRatio = Math.min(2, window.devicePixelRatio);
+
+	renderer.setSize(size.width, size.height);
+
+	camera.aspect = size.width / size.height;
+	camera.updateProjectionMatrix();
+}
+window.addEventListener('resize', resize);
