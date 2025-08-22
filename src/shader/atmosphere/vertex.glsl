17a4ccb1a7d9d5f35a2c8d1a3c6e2fa341049736
LeomharT <leo.liao.lzy@gmail.com>
2025-08-22 15:50:00 +0800

    clear: clean code

--- src/shader/atmosphere/vertex.glsl
+++ src/shader/atmosphere/vertex.glsl
@@ -1,16 +1,3 @@
-varying vec3 vPosition;
-varying vec3 vNormal;
-
-void main(){
-    vec4 modelPosition      = modelMatrix * vec4(position, 1.0);
-    vec4 modelNormal        = modelMatrix * vec4(normal, 0.0);
-    vec4 viewPosition       = viewMatrix * modelPosition;
-    vec4 projectionPosition = projectionMatrix * viewPosition;
-
-    // Final position
-    gl_Position = projectionPosition;
-
-    // Varying
-    vNormal   = modelNormal.xyz;
-    vPosition = modelPosition.xyz;
+void main() {
+    
 }
